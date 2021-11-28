a93d563889ec9abe8e00a887ef9a86844792a1c4
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2021-11-28 11:48:06 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,53 +1,36 @@
-import {createAppContainer} from "react-navigation";
-import {createStackNavigator} from "react-navigation-stack";
-import { createSwitchNavigator } from "react-navigation";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import TutorialScreen from "./src/screens/TutorialScreen";
-import SplashScreen from "./src/screens/SplashScreen";
-import EditProfileScreen from "./src/screens/EditProfileScreen";
-import TabNavigationRoutes from "./src/components/TabNavigationRoutes";
-
-
-// A Stack Navigator for authentication screens and tutorial 
-const AuthStack = createStackNavigator({
-	Tutorial: TutorialScreen,
-	Login: {
-		screen: LoginScreen,
-		navigationOptions: {
-			headerShown: false
-		}
-	},
-	Signup: {
-		screen: SignUpScreen,
-		navigationOptions: {
-			title: "",
-			headerTransparent: true
-		}
-	},
+import ResetPasswordScreen from "./src/screens/ResetPasswordScreen";
+import UserScreen from "./src/screens/UserScreen";
+import UniqueKeyScreen from "./src/screens/UniqueKeyScreen"
+import TutorialOptionScreen from "./src/screens/TutorialOptionScreen";
+import VideoPlayerScreen from "./src/screens/VideoPlayerScreen";
+import IndexScreen from "./src/screens";
+const navigator = createStackNavigator(
+  
+  {
+   Welcome: WelcomeScreen,
+  Tutorial: TutorialScreen,
+  Login: LoginScreen,
+  Signup: SignUpScreen,
+  reset:ResetPasswordScreen,
+  user:UserScreen,
+  unique:UniqueKeyScreen,
+  option:TutorialOptionScreen,
+  player:VideoPlayerScreen,
+  index:IndexScreen
 },
-{
-	initialRouteName: "Login",
-	
+  {
+    initialRouteName: "index",
+    defaultNavigationOptions: {
+      title: "GAMENET",
+      //headerLeft:""
+    }
 
-});
+  });
 
-export default createAppContainer(
-	createSwitchNavigator({
-		// Plays for 5 seconds on the opening of the app 
-		Splash: {
-			screen: SplashScreen,
-		},
-		//Stack Navigation route
-		Auth: {
-			screen: AuthStack
-		},
-		//Tab Navigation route
-		Tab: {
-			screen: TabNavigationRoutes
-		},
-	},
-	{
-		initialRouteName: "Splash",
-	})
-);
+export default createAppContainer(navigator);
