54e4ce344c4bbd7bdc81c9f060d76f21ef1fb677
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2021-12-06 12:37:26 -0600

    Update TutorialOptionScreen.js
    i copy and pasted bout to add video in assets

--- src/screens/TutorialOptionScreen.js
+++ src/screens/TutorialOptionScreen.js
@@ -8,7 +8,8 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 let screenNavigation = {
-    screen:''
+    route:'',
+    screenTitle:''
 
     }
     let screen = ''
@@ -20,11 +21,10 @@ let screenNavigation = {
         Signup: 'SignUpScreen',
         reset:'Reset Screen',
         user:'UserScreen',
-        unique:'UniqueKeyScreen',
+        unique:'unique code',
         option:'Tutorials',
         player:'VideoPlayerScreen',
         index:'IndexScreen'
-    
     }
     
     let route = ''
@@ -32,64 +32,35 @@ let screenNavigation = {
 const TutorialOptionScreen = function (props) {
 
     route = props.navigation.getParam('screen')
-    screenNavigation.screen = route
-    screen = screenPrompt[route]
+    screenNavigation.route = route
+    screenNavigation.screenTitle = screenPrompt[route]
 
-const optionArray = [{screen:route,text:'How to create a new account',video :require('../../assets/tutorialvideos/vidaccount1.mp4')},
-                    {screen:route,text:'How to reset your password',video :require('../../assets/tutorialvideos/vidaccount2.mp4')},
-                    {screen:route,text:'How to log into your account',video :require('../../assets/tutorialvideos/vidaccount3.mp4')}
+const optionArray = [{screen:route,text:'How to create a new account',video :require('../../assets/tutorialvideos/createaccount.mp4')},
+                    {screen:route,text:'How to reset your password',video :require('../../assets/tutorialvideos/forgotpassword.mp4')},
+                    {screen:route,text:'How to logout of GameNet',video :require('../../assets/tutorialvideos/logout.mp4')},
+                    {screen:route,text:'How to Sign into account',video :require('../../assets/tutorialvideos/signinaccount.mp4')},
+                    {screen:route,text:'How to search for a game',video :require('../../assets/tutorialvideos/gamesearch.mp4')},
+                    {screen:route,text:'How to view game details',video :require('../../assets/tutorialvideos/gameinfo.mp4')},
+                    {screen:route,text:'How to save a game',video :require('../../assets/tutorialvideos/savegame.mp4')}
 ]
     return <View style={styles.container}>
-        	<View style={styles.circleShape}>
-
-
-<Video
-
-	style={styles.circleicon}
-	source={require('../../assets/logo/GameNet2.mp4')}
-	isLooping ={true}
-	//useNativeControls
-	resizeMode="contain"
-	shouldPlay
-
-
-/>
-</View>
-<View style = {styles.iconView}>
-<TouchableOpacity onPress = {() =>{
-    props.navigation.navigate(route)
-}}>
-<Ionicons name='arrow-back-circle'
-          style={{color:'green',marginLeft:300,fontSize:50,}}
-          />
-</TouchableOpacity>
-
-</View>
+            
         <Text style={styles.header}>Tutorial videos</Text>
 
         <ScrollView>
-            <TutorialHeader text='User Account' />
+            <TutorialHeader text='Authentication' />
             <TutorialData text={optionArray[0].text}  handler={()=>{ props.navigation.navigate('player',optionArray[0])}}/>
             <TutorialData text={optionArray[1].text}  handler={()=>{ props.navigation.navigate('player',optionArray[1])}}/>
-            <TutorialData text={optionArray[2].text}  handler={()=>{ props.navigation.navigate('player',optionArray[2])}}/>
+            <TutorialData text={optionArray[3].text}  handler={()=>{ props.navigation.navigate('player',optionArray[3])}}/>
 
         
-            <TutorialHeader text='????????' />
-            <TutorialData text='??????????' handler={()=>{ props.navigation.navigate('player')}}/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialHeader text='????????' />
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-            <TutorialData text='??????????'/>
-
-           
+            <TutorialHeader text='User Account' />
+            <TutorialData text={optionArray[2].text}  handler={()=>{ props.navigation.navigate('player',optionArray[2])}}/>
+            <TutorialData text={optionArray[4].text}  handler={()=>{ props.navigation.navigate('player',optionArray[4])}}/>
+            <TutorialData text={optionArray[5].text}  handler={()=>{ props.navigation.navigate('player',optionArray[5])}}/>    
+            <TutorialData text={optionArray[6].text}  handler={()=>{ props.navigation.navigate('player',optionArray[6])}}/>
+        
         </ScrollView>
-
     </View>
 }
 
@@ -98,6 +69,7 @@ const styles = StyleSheet.create({
         flex: 1,
         //justifyContent: "center",
         backgroundColor: "#e5e5e5",
+        //borderWidth: 3
     },
    
     header: {
@@ -134,24 +106,32 @@ const styles = StyleSheet.create({
 		borderRadius: 50 / 2,
 		alignSelf: 'center',
         justifyContent:'center'
-
 	},
-	
 	signinStyle: {
 		alignSelf: 'center',
 		fontSize: 40,
 		marginBottom: 12,
 		color:'skyblue'
-
 	},
     iconView:{
         top:-27,
         marginLeft:30
-
     }
 
 });
 
 export default TutorialOptionScreen;
 
+/* Not needed
+<View style = {styles.iconView}>
+                <TouchableOpacity onPress = {() =>{
+                    props.navigation.navigate(route)}}
+                >
+                    <Ionicons 
+                        name='arrow-back-circle'
+                        style={{color:'green',marginLeft:300,fontSize:50,}}
+                    />
+                </TouchableOpacity>
 
+            </View>
+*/
